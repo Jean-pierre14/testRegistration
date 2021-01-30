1b8ed1a3bd0fc62658dd50b7ebc159a739c733ab
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-01-30 15:37:13 +0200

    Get user

--- routes/pages.js
+++ routes/pages.js
@@ -4,12 +4,14 @@ const User = require('../models/userSchema')
 
 router.use('/student', require('./Student'))
 
+
 router.get('/', (req, res) => {
     User.find((results) => {
         res.json(results)
     })
 })
 router.get('/get/:id', (req, res) => {
+    let id = req.params.id
     User.findById(id, (result) => {
         res.json(result)
     })
