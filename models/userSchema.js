5e6d9dd7aa8f96a77f5048926ea7db420f0c01ff
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-01-30 15:31:21 +0200

    UserSchema

---
+++ models/userSchema.js
@@ -0,0 +1,39 @@
+const db = require('../config/db')
+class User{
+    static find(cb) {
+        let sql = "SELECT * FROM users ORDER BY id"
+        db.query(sql, (err, results) => {
+            if (err) throw err
+            cb(results)
+        })
+    }
+    static findById(id, cb) {
+        let sql = "SELECT * FROM users WHERE id = ?"
+        db.query(sql, [id], (err, result) => {
+            if (err) throw err
+            cb(result)
+        })
+    }
+    static UpdateById(id, cb) {
+        let sql = `UPDATE users SET username = ?, fullname, password = ? WHERE id = ?`
+        db.query(sql, [username, fullname, password, id], (err, result) => {
+            if (err) throw err
+            cb(result)
+        })
+    }
+    static DeleteById(id, cb) {
+        let sql = `DELETE FROM users WHERE id = ?`
+        db.query(sql, [id], (err, result) => {
+            if (err) throw err
+            cb(result)
+        })
+    }
+    static DeleteAll(cb) {
+        let SQL = `DELETE FROM users`
+        db.query(SQL, (err, results) => {
+            if (err) throw err
+            cb(results)
+        })
+    }
+}
+module.exports = User
--- routes/Student.js
+++ routes/Student.js
@@ -2,17 +2,11 @@ const exp = require('express')
 const router = exp.Router()
 const User = require('../models/userSchema')
 
-router.use('/student', require('./Student'))
-
 router.get('/', (req, res) => {
     User.find((results) => {
         res.json(results)
     })
 })
-router.get('/get/:id', (req, res) => {
-    User.findById(id, (result) => {
-        res.json(result)
-    })
-})
+
 
 module.exports = router
